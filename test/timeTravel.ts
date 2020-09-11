25de9eed9b8b84c5cd0d14cd9d53c9e586a57863
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2020-09-11 13:57:46 +0200

    fix timeTravel

--- test/timeTravel.ts
+++ test/timeTravel.ts
@@ -1,27 +1,25 @@
-const m = async (web3: any, time: number) => {
+const sendRPCCall = async (web3: any, method: string, parameters: any[] = []) => {
   return new Promise((resolve, reject) => {
     web3.currentProvider.send({
       jsonrpc: "2.0",
-      method: "evm_mine",
-      params: [time],
+      method: method,
+      params: parameters,
       id: new Date().getTime(),
     }, (err: Error, _: any) => {
       if (err) {
         return reject(err);
       }
-      const newBlockHash = web3.eth.getBlock("latest").hash;
-
-      return resolve(newBlockHash);
+      return resolve();
     });
   });
 }
 
-export const mine = async (web3: any, times: number) => {
+export const mine = async (web3: any, times: number = 1) => {
   for (let i = 0; i < times; i++) {
-    await m(web3, Date.now())
+    await sendRPCCall(web3, "evm_mine")
   }
 }
 
-export const timeTravel = async (web3: any, to: number) => {
-  await m(web3, to)
+export const timeTravel = async (web3: any, seconds: number) => {
+  await sendRPCCall(web3, "evm_increaseTime", [seconds])
 }
